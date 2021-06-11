bf532f846eb8034854d9c897bc02b6c9fd3faf20
GitHub Action <action@github.com>
2021-06-11 01:47:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lijialing888/myawesomeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lijialing888.myawesomeplugin.services
+
+import com.github.lijialing888.myawesomeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/lijialing888/myawesomeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lijialing888/myawesomeplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lijialing888.myawesomeplugin.services
 
+import com.github.lijialing888.myawesomeplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
